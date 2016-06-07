607ed04f7a16db11302e9a1c4671a8c63b362a9f
Inioluwa <adeb6600@gmail.com>
2016-06-07 10:08:42 +0100

    added search parameters for person and projects

--- api/controllers/SearchController.js
+++ api/controllers/SearchController.js
@@ -5,6 +5,9 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var Promise = require('bluebird');
+var pager = require('sails-pager');
+
 module.exports = {
 
     /*
@@ -14,69 +17,93 @@ module.exports = {
      * 
      */
     search: function(req, res) {
-
         var query = req.query.query
-        console.log('query value');
-        console.log(query)
-        var searchResult = [];
-        /*
-        preprocess query 
-         */
+        async.parallel({
+            person: function(callback) {
+                setTimeout(function() {
+                    sails.controllers.search.searchPerson(query, function(err, result) {
+                        if (err) {
+                            callback(err);
+                        }
+                        callback(null, result)
 
-        /*
-        search the  qualified model based on returned preprocessed query
-         */
-        //search person 
-        Person.native(function(err,collection){
-            if(err){
+                    })
+                }, 100)
+            },
+            project: function(callback) {
+                 setTimeout(function() {
+                    sails.controllers.search.searchProject(query, function(err, result) {
+                        if (err) {
+                            callback(err);
+                        }
+                        callback(null, result)
+
+                    })
+                }, 100)
+            }
+        }, function(err, results) {
+            if (err) {
+                return ResponseService.json(400, res, "Error Retrieving search results")
+            }
+            return ResponseService.json(200, res, " Search Results Retrieved Successfully", results);
+        })
+
+    },
+
+    searchPerson: function(query, cb) {
+        var searchResult = [];
+        Person.native(function(err, collection) {
+            if (err) {
                 console.log(err)
             }
-            collection.find({$text: {$search: query}}, { score: { $meta: "textScore" } })
-           .sort( { score: { $meta: "textScore" } } ).toArray(function(err , persons){
-                if(err){
-                    console.log(err)
-                    console.log('err finding person')
-           return ResponseService.json(200,res , "Persons not found " , searchResult)
-   
-                }
-              persons.forEach(function(person) {
+            collection.find({ $text: { $search: query } }, { score: { $meta: "textScore" } })
+                .sort({ score: { $meta: "textScore" } }).toArray(function(err, persons) {
+                    if (err) {
+                        console.log(err)
+
+                        console.log('err finding person')
+                        cb(err)
+                        return ResponseService.json(200, res, "Persons not found ", searchResult)
+
+                    }
+                    persons.forEach(function(person) {
                         person.dataType = 'person';
                         searchResult.push(person)
                     })
-
-              return ResponseService.json(200,res , "Persons retrieved successfully" , searchResult)
-            })
+                    cb(null, searchResult)
+                })
         })
-        // Person.find({ $text: { 'search': "bukola" } }).then(function(person) {
+    },
+    searchProject: function(query , cb) {
+      var searchResult = [];
+        Project.native(function(err, collection) {
+            if (err) {
+                console.log(err)
+            }
+            collection.find({ $text: { $search: query } }, { score: { $meta: "textScore" } })
+                .sort({ score: { $meta: "textScore" } }).toArray(function(err, projects) {
+                    if (err) {
+                        console.log(err)
 
-        //         var projectQry = Project.find({$text: {'search': "bukola"}});
+                        console.log('err finding project')
+                        cb(err)
+                        return ResponseService.json(200, res, "Project not found ", searchResult)
 
-        //         return [person, projectQry];
-        //     }).spread(function(persons, projects) {
-        //         if (persons.length) {
-        //             persons.forEach(function(person) {
-        //                 person.dataType = 'person';
-        //                 searchResult.push(person)
-        //             })
-        //         }
-        //         if (projects.length) {
-        //             projects.forEach(function(project) {
-        //                 project.dataType = 'project'
-        //                 searchResult.push(project)
-        //             })
-        //         }
-        //         if (searchResult.length) {
-        //             return ResponseService.json(200, res, "Data Retrieved successfully", searchResult)
-        //         }
-        //         return ResponseService.json(200, res, "Data not Found", searchResult);
-        //     }).catch(function(err) {
-        //         ValidationService.jsonResolveError(err, res)
-        //     })
-            /*
-            return an array of result
-             */
-            /*
-            create 
-             */
-    }
+                    }
+                    projects.forEach(function(project) {
+                        project.dataType = 'project';
+                        searchResult.push(project)
+                    })
+                    cb(null, searchResult)
+                })
+        })  },
+    searchDistrict: function(query) {
+
+    },
+    searchConstituency: function(query) {
+
+    },
+
+
+
 };
